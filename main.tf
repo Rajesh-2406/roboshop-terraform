3de65104483f749f7a8ed8fa8d0702e6e44010e2
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-07-27 14:57:17 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "test" {
-  source =  "https://github.com/Rajesh-2406/terraform-module-application.git"
+module  "test"{
+  source  = "git::https://github.com/Rajesh-2406/terraform-module-application.git"
   env = "dev"
 }
