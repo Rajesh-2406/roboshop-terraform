81ac7a1ab7b1e3a41060818aa4f1aa881e2506b9
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-07 10:49:01 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-module  "test"{
+module  "intances"{
   for_each  = var.components
   source  = "git::https://github.com/Rajesh-2406/terraform-module-application.git"
   component = each.key
-  env = "dev"
+  env = var.env
 }
