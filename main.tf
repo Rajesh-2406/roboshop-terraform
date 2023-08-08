ba5e33e8de29899d3e22567ce8cd0479885a5958
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-08 07:36:28 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module  "instance" {
   for_each = var.components
   source  = "git::https://github.com/Rajesh-2406/terraform-module-application.git"
-  component  = each.key
+  components  = each.key
   env = var.env
 }
