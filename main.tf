f220b6c70bcb74b129fdddc3168be80571b6a12b
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-26 11:59:02 +0530

    ansible automation

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ module "vpc" {
   source     = "git::https://github.com/Rajesh-2406/tf-module-vpc.git"
   for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
+  web_subnet_cidr_block = each.value["web_subnet_cidr_block"]
+
+  env = var.env
+  tags = var .tags
 }
