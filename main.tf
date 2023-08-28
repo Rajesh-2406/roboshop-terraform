50551c9713c07c570eeba76167342d3ff0e5ffe3
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-28 10:00:35 +0530

    vpc subnet

--- main.tf
+++ main.tf
@@ -14,13 +14,12 @@ module "vpc" {
 
 /*
 module "app_server" {
-  source = "git::https://github.com/Rajesh-2406/terraform-module-application.git"
+  source ="git::https://github.com/Rajesh-2406/terraform-module-application.git"
 
-  env       = var.env
-  tags      = var.tags
+  env = var.env
+  tags = var.tags
   component = "test"
-  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids",null), "app", null),"subnet_ids",null)[0]
-  vpc_id = lookup(lookup(module.vpc, "main",null), "vpc_id",null)
+  subnet_id =
+
 }
 */
-
