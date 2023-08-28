e4cae860d8814ffd35dd68a1c50af10a6f52b4cb
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-28 10:26:49 +0530

    vpc ceration

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "vpc" {
 }
 
 
-module "app_server" {
+m/*odule "app_server" {
   source = "git::https://github.com/Rajesh-2406/terraform-module-application.git"
 
   env       = var.env
@@ -20,4 +20,4 @@ module "app_server" {
   component = "test"
   subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "app", null), "subnet_ids", null)[0]
   vpc_id = lookup(lookup(module.vpc, "main",null ),"vpc_id",null)
-}
+}*/
