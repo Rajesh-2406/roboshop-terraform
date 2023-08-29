341730016a8086009b33316a0e32792a3358cc87
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-29 16:29:07 +0530

    vpc ceration

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "rds"{
   engine         = each.value["engine"]
   engine_version =each.value["engine_version"]
   database_name  = each.value["database_name"]
-  subnet_id      = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+  subnet_ids      = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
   instance_count = each.value["instance_count"]
   instance_class = each.value["instance_class"]
   vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
