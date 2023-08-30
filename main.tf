fa8cffa030556f5c800eb1fe0150da0b86cf7bd1
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-30 12:47:39 +0530

    vpc ceration

--- main.tf
+++ main.tf
@@ -73,8 +73,9 @@ module "elasticache" {
   num_node_groups         = each.value["num_node_groups"]
   node_type               = each.value["node_type"]
   subnet_ids              = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-  vpc_id                  = lookup(lookup(module.vpc, "main", null), "vpc_id", null)  env                     = var.env
+  vpc_id                  = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
 
+  env                     = var.env
   tags                    = var.tags
   kms_key_arn             = var.kms_key_arn
   sg_subnet_cidr          = lookup(lookup(lookup(lookup(var.vpc, "main",null),"subnets", null), "app", null), "cidr_block", null)
