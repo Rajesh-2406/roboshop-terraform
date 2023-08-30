42e169d03217b898b0a09205c8c9e34b91165b41
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-30 15:07:00 +0530

    vpc ceration

--- main.tf
+++ main.tf
@@ -47,12 +47,12 @@ module "documentdb" {
 
    for_each           = var.rds
    component          = each.value["component"]
-   subnet_ids         = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-   vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
    engine             = each.value["engine"]
    engine_version     = each.value["engine_version"]
    db_instance_count  = each.value["db_instance_count"]
    instance_class     = each.value["instance_class"]
+  subnet_ids         = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+  vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
 
 
    env                = var.env
