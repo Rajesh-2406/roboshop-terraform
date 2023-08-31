5ed1f347a4065dbb16d34939aa5f6f1b2646f178
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-08-31 14:28:09 +0530

    apps creation

--- main.tf
+++ main.tf
@@ -149,5 +149,6 @@ module "apps" {
   tags = var.tags
   kms_key_id = var.kms_key_arn
   allow_ssh_cidr = var.allow_ssh_cidr
+  kms_arn = var.kms_key_arn
   allow_prometheus_cidr = var.allow_prometheus_cidr
 }
