7e9847d9d36cdcd6877d0aeda08ed5c755191702
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-09-03 10:24:05 +0530

    terraform project

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "rds" {
   component          = each.value["component"]
   engine             = each.value["engine"]
   engine_version     = each.value["engine_version"]
-  database_name      = each.value["database_name"]
+  db_name      = each.value["db_name"]
   instance_class     = each.value["instance_class"]
   subnet_ids         = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
   vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
