efff02920dfe25d75df5cc4a59d0e50038fa5446
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-10-08 10:24:48 +0530

    terraform

--- load-runner.tf
+++ load-runner.tf
@@ -1,4 +1,5 @@
-/**/data "aws_ami" "ami" {
+
+/*data "aws_ami" "ami" {
   owners      = ["973714476881"]
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
@@ -22,4 +23,4 @@ provisioner "remote-exec"{
           "docker pull roboshop/rs-load:Latest"
         ]
 }
-}/**/
+}*/
